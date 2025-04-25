3f188fe91afa8ea42ccbde6cb91ccd009a1c9a96
Saba-Developer <sabamadina87@gmail.com>
2025-04-25 12:10:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let radius = parseInt(prompt("Enter the Radius of the circle : "));
+let diameter = 2*radius;
+let circumference = 2 * Math.PI * radius;
+let area = Math.PI * radius * radius;
+console.log(`The Diameter of Circle : ${diameter}`);
+console.log(`The Circumference of Circle : ${circumference}`);
+console.log(`The Area of Circle : ${area}`);
